ba025bb512bb5d715414c3284f9234f98266a287
LHO-Developments <LHO_Developments@outlook.com>
2025-06-07 02:00:59 -0500

    Added scripts
    Add scripts to level1 and player

---
+++ Speedy Saucer/speedy-saucer/level_1.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time
+func _ready():
+	pass #replace with function body
+	
+# Called every time. 'delta' is elasped time since the previous frame
+func _process(delta):
+	pass
