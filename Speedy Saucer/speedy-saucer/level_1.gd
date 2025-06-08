f5220eae84a2733a7f6a9f1b19a47b0729087839
LHO-Developments <LHO_Developments@outlook.com>
2025-06-07 22:46:09 -0500

    Create maze + controls + camera
    Create maze scene + controls input + camera movement

--- Speedy Saucer/speedy-saucer/level_1.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-# Called when the node enters the scene tree for the first time
-func _ready():
-	pass #replace with function body
-	
-# Called every time. 'delta' is elasped time since the previous frame
-func _process(delta):
-	pass
--- Speedy Saucer/speedy-saucer/player.gd
+++ Speedy Saucer/speedy-saucer/player.gd
@@ -1,9 +1,13 @@
 extends RigidBody2D
 
-# Called when the node enters the scene tree for the first time
-func _ready():
-	pass #replace with function body
-	
-# Called every time. 'delta' is elasped time since the previous frame
-func _process(delta):
-	pass
+var force = 1000;
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force,0));
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force,0));
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0,-force));
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0,force));
