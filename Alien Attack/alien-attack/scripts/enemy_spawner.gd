cca4c0c48697303a00a4ed1e862146ef787cab7c
LHO-Developments <LHO_Developments@outlook.com>
2025-06-14 22:37:04 -0500

    spawn enemy at random

--- Alien Attack/alien-attack/scripts/enemy_spawner.gd
+++ Alien Attack/alien-attack/scripts/enemy_spawner.gd
@@ -1,4 +1,21 @@
 extends Node2D
 
+var enemy_scene = preload("res://scenes/enemy.tscn");
+
+@onready var spawn_positions = $SpawnPositions;
+
 func _on_timer_timeout() -> void:
-	pass;
+	spawn_enemy();
+
+func spawn_enemy() -> void:
+	# get an array of children of enemy markers
+	var spawn_position_array = spawn_positions.get_children();
+	
+	# pick random marker to spawn the enemy
+	var random_spawn_position = spawn_position_array.pick_random();
+	
+	# create instance of enemy obj
+	var enemy_instance = enemy_scene.instantiate();
+	enemy_instance.global_position = random_spawn_position.global_position;
+	add_child(enemy_instance);
+	
