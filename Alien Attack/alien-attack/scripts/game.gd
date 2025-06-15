000c23dc5c8fb6dd1b955a9664e86819c4b127a5
LHO-Developments <LHO_Developments@outlook.com>
2025-06-15 15:50:39 -0500

    add score to game
    create custom to detect enemy death via game scene when enemy arent there but from enemy spawner

--- Alien Attack/alien-attack/scripts/game.gd
+++ Alien Attack/alien-attack/scripts/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @export var lives = 3;
+var score = 0;
 @onready var player = $Player;
 # delte enemy when hit enemy zone
 func _on_deathzone_area_entered(area: Area2D) -> void:
@@ -13,3 +14,12 @@ func _on_player_took_damage() -> void:
 		player.die();
 	else:
 		print(lives);
+
+# add access to enemy when enemy not in game
+func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
+	enemy_instance.connect("died",_on_enemy_died);
+	add_child(enemy_instance);
+
+func _on_enemy_died():
+	score += 100;
+	print("Score: " + str(score));
