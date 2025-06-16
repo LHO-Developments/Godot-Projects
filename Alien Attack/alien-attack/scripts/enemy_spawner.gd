518bd1689848901647be92196a821b9d02961d4a
LHO-Developments <LHO_Developments@outlook.com>
2025-06-15 21:59:18 -0500

    Add new enemy

--- Alien Attack/alien-attack/scripts/enemy_spawner.gd
+++ Alien Attack/alien-attack/scripts/enemy_spawner.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 signal enemy_spawned(enemy_instance);
+signal path_enemy_spawned(path_enemy_instance);
 
 var enemy_scene = preload("res://scenes/enemy.tscn");
+
+var path_enemy_scene = preload("res://scenes/path_enemy.tscn");
 
 @onready var spawn_positions = $SpawnPositions;
 
@@ -21,3 +24,11 @@ func spawn_enemy() -> void:
 	enemy_instance.global_position = random_spawn_position.global_position;
 	emit_signal("enemy_spawned", enemy_instance);
 	
+
+
+func _on_path_timer_timeout() -> void:
+	spawn_path_enemy();
+
+func spawn_path_enemy():
+	var path_enemy_instance = path_enemy_scene.instantiate();
+	emit_signal("path_enemy_spawned", path_enemy_instance);
