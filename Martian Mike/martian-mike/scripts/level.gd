60f03702f0bf5794021a214f1072cbc7bc6aa31d
LHO-Developments <LHO_Developments@outlook.com>
2025-06-22 15:34:46 -0500

    updates
    - Add start position
    - Add death zone
    - Reset player position to start position
    - update camera

--- Martian Mike/martian-mike/scripts/level.gd
+++ Martian Mike/martian-mike/scripts/level.gd
@@ -1,7 +1,15 @@
 extends Node
 
+@onready var start_position = $StartPosition;
+
 func _process(delta: float) -> void:
 	if(Input.is_action_just_pressed("quit")):
 		get_tree().quit();
 	elif(Input.is_action_just_pressed("reset")):
 		get_tree().reload_current_scene();
+
+
+func _on_deathzone_body_entered(body: Node2D) -> void:
+	body.velocity = Vector2.ZERO;
+	body.global_position = Vector2(100, 100);
+	body.global_position = start_position.global_position;
