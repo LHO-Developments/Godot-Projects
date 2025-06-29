28d3578908dee6747ae4b356c8ee3ad8d0a75975
LHO-Developments <LHO_Developments@outlook.com>
2025-06-29 16:02:30 -0500

    add start menu

--- Martian Mike/martian-mike/scripts/win_screen.gd
+++ Martian Mike/martian-mike/scripts/win_screen.gd
@@ -4,4 +4,4 @@ extends Control
 
 #reset to first level
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level.tscn");
+	get_tree().change_scene_to_file("res://scenes/start_menu.tscn");
