6ac14563d4f1201a17d52b0ebb45567b7cb05e4f
LHO-Developments <LHO_Developments@outlook.com>
2025-07-08 21:47:52 -0500

    Add main scene + signal

--- Flappy Plane/flappy-plane/Player/player.gd
+++ Flappy Plane/flappy-plane/Player/player.gd
@@ -1,20 +1,31 @@
 extends CharacterBody2D
 
+signal  on_game_started;
+
 @export var gravity: float = 1000;
 @export var jump_force: float = -400;
 @export var max_speed: float = 400;
 @export var rotation_speed: float = 2;
 
+var is_started: bool = false;
+var should_process_input: bool = true;
+
 func _physics_process(delta: float) -> void:
+		# Check for jump input
+	if Input.is_action_just_pressed("jump") && should_process_input:
+		velocity.y = jump_force;
+		rotation = deg_to_rad(-30);
+		if !is_started:
+			is_started = true;
+			on_game_started.emit();
+	
+	if !is_started:
+		return ;
+		
 	
 	# apply gravity
 	velocity.y += gravity * delta;
 	velocity.y = min(velocity.y, max_speed);
-	
-	# Check for jump input
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = jump_force;
-		rotation = deg_to_rad(-30);
 	
 	# move the player
 	move_and_slide();
