4800eb0b8583f9894edaff5b5aa5f840fd48fad4
LHO-Developments <LHO_Developments@outlook.com>
2025-07-17 22:19:21 -0500

    create cashier scene + animation for cashier

---
+++ Idle Food Stand/idle-food-stand/Scenes/cashier/cashier.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name Cashier;
+
+@export var move_speed := 50.0;
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer;
+
+func move_to_customer() -> void:
+	animation_player.play("Move");
+	
+func move_to_item_position() -> void:
+	animation_player.play("Idle");
