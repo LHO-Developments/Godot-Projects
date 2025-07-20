0bff83e22d6b6270ab4865cddf27b4886143fa04
LHO-Developments <LHO_Developments@outlook.com>
2025-07-20 00:10:10 -0500

    Add customer manager node to spawn customers

--- Idle Food Stand/idle-food-stand/Scenes/customer/customer.gd
+++ Idle Food Stand/idle-food-stand/Scenes/customer/customer.gd
@@ -5,6 +5,7 @@ class_name Customer;
 @onready var face: Sprite2D = %Face
 @onready var hand_left: Sprite2D = %HandLeft
 @onready var hand_right: Sprite2D = %HandRight
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 func set_sprites(data: CustomerData) -> void:
 	body.texture = data.body;
@@ -12,3 +13,6 @@ func set_sprites(data: CustomerData) -> void:
 	hand_left.texture = data.hand;
 	hand_right.texture = data.hand;
 	
+
+func play_move_anim() -> void:
+	anim_player.play("Move");
---
+++ Idle Food Stand/idle-food-stand/Scenes/managers/customer_manager.gd
@@ -0,0 +1,31 @@
+extends Node
+class_name CustomerManager;
+
+@export var spawn_positions: Array[Marker2D];
+@export var customer_sprites: Array[CustomerData];
+@export var customer_scene: PackedScene;
+
+func _ready() -> void:
+	spawn_customer();
+
+func spawn_customer() -> void:
+	var customer: Customer = customer_scene.instantiate();
+	add_child(customer);
+	
+	#get random sprites
+	var sprite_data: CustomerData = customer_sprites.pick_random();
+	customer.set_sprites(sprite_data);
+	
+	#get random spawn pos
+	var random_start_pos: Marker2D = spawn_positions.pick_random();
+	customer.position = random_start_pos.position;
+	customer.play_move_anim();
+	
+	#make customer move right and then delete after
+	var tween: Tween = create_tween();
+	tween.tween_property(customer,"position",customer.position + Vector2.RIGHT * 1350,5);
+	tween.finished.connect(func(): customer.queue_free());
+
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_customer();
