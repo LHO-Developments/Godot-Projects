a9916cf06025e8712b3032fb2742abee9fa79a13
LHO-Developments <LHO_Developments@outlook.com>
2025-08-23 22:11:50 -0500

    Add spike block scene, layers, autoload script

---
+++ Wall Platformer/wall-platformer/scenes/obstacles/spike_block.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+class_name SpikeBlock;
+
+@export var rotation_interval: float = 2.0;
+
+var timer: float;
+
+func _process(delta: float) -> void:
+	timer += delta;
+	if timer >= rotation_interval:
+		do_rotation();
+		timer = 0.0;
+
+func do_rotation() -> void:
+	var current_rot = rotation_degrees;
+	var desired_rot = current_rot - 90;
+	var tween := create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT);
+	tween.tween_property(
+		self,
+		"rotation_degrees",
+		desired_rot,
+		0.5
+	);
+	
