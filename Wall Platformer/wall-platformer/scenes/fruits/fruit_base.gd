dccf017d55e587b6170e674158050e6df22a409e
LHO-Developments <LHO_Developments@outlook.com>
2025-08-24 17:21:02 -0500

    add collectable item

---
+++ Wall Platformer/wall-platformer/scenes/fruits/fruit_base.gd
@@ -0,0 +1,15 @@
+extends Area2D
+class_name Fruit;
+
+var collected: bool = false;
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is not Player: return;
+	if collected: return;
+	
+	collected = true;
+	anim_sprite.play("collected");
+	EventManager.on_fruit_collected.emit();
+	await  get_tree().create_timer(0.5).timeout;
+	queue_free();
