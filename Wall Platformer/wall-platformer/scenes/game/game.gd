4dbe66aa97d0bef2b586b0a61ad42c3eb13415da
LHO-Developments <LHO_Developments@outlook.com>
2025-09-08 23:10:57 -0500

    Add game ui

--- Wall Platformer/wall-platformer/scenes/game/game.gd
+++ Wall Platformer/wall-platformer/scenes/game/game.gd
@@ -7,6 +7,9 @@ class_name Game;
 var points: int;
 var checkpoint_reached: bool;
 
+@onready var game_won: Panel = %GameWon
+@onready var score_label: Label = %ScoreLabel
+
 func _ready() -> void:
 	EventManager.on_player_dead.connect(_on_player_dead);
 	EventManager.on_fruit_collected.connect(_on_fruit_collected);
@@ -30,9 +33,14 @@ func _on_player_dead() -> void:
 
 func _on_fruit_collected() -> void:
 	points += 1;
+	score_label.text = str(points);
 
 func _on_checkpoint_reached() -> void:
 	checkpoint_reached = true;
 
 func _on_game_won() -> void:
-	print('GAME WON!')
+	game_won.show();
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().reload_current_scene();
