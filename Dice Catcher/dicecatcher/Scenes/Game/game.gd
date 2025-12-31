21f833b2f4d1a9d5c5aea84d0a2bc95dcd576cf0
LHO-Developments <LHO_Developments@outlook.com>
2025-12-30 18:20:07 -0600

    add music + new pause

--- Dice Catcher/dicecatcher/Scenes/Game/game.gd
+++ Dice Catcher/dicecatcher/Scenes/Game/game.gd
@@ -3,13 +3,21 @@ extends Node2D
 const DICE = preload("res://Scenes/Dice/dice.tscn");
 const MARGIN: float = 80.0;
 const STOPPABLE_GROUP: String = "stoppable";
-@onready var spawn_timer: Timer = $SpawnTimer
+@onready var spawn_timer: Timer = $Pausable/SpawnTimer;
+
+var _points: int = 0;
+@onready var score_label: Label = $ScoreLabel
+const GAME_OVER = preload("res://Asset/game_over.wav");
+@onready var music: AudioStreamPlayer = $Music;
+@onready var pausable: Node = $Pausable;
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene();
 
 func _ready() -> void:
+	get_tree().paused = false;
+	update_score_label();
 	spawn_dice();
 	
 
@@ -22,7 +30,7 @@ func spawn_dice() -> void:
 	)
 	new_dice.position = Vector2(new_x, -MARGIN);
 	new_dice.game_over.connect(_on_dice_game_over);
-	add_child(new_dice);
+	pausable.add_child(new_dice);
 
 func pause_all() -> void:
 	spawn_timer.stop();
@@ -30,12 +38,19 @@ func pause_all() -> void:
 	for item in to_stop:
 		item.set_physics_process(false);
 
-func _on_dice_game_over() -> void:
-	pause_all();
+func update_score_label() -> void:
+	score_label.text = "%04d" % _points;
 
+func _on_dice_game_over() -> void:
+	#pause_all();
+	music.stop();
+	music.stream = GAME_OVER;
+	music.play();
+	get_tree().paused = true;
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_dice();
 
 func _on_fox_score() -> void:
-	print("SCORED")
+	_points += 1;
+	update_score_label();
