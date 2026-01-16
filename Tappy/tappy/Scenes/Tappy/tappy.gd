ee500b9eaf290040496f976ae6cb8bffcb1c9213
LHO-Developments <LHO_Developments@outlook.com>
2026-01-15 20:18:35 -0600

    add jump animation to plane

--- Tappy/tappy/Scenes/Tappy/tappy.gd
+++ Tappy/tappy/Scenes/Tappy/tappy.gd
@@ -7,22 +7,27 @@ const JUMP_POWER: float = -350.0;
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity");
 var _jumped: bool = false;
+@onready var animation_player: AnimationPlayer = $AnimationPlayer;
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("power"):
 		_jumped = true;
 
 func _physics_process(delta: float) -> void:
-	velocity.y += _gravity * delta;
 	
-	if _jumped:
-		velocity.y = JUMP_POWER;
-		_jumped = false;
-	
+	fly(delta);
 	move_and_slide();
 	
 	if is_on_floor():
 		die();
+
+func fly(delta: float) -> void:
+	velocity.y += _gravity * delta;
+	
+	if _jumped:
+		velocity.y = JUMP_POWER;
+		animation_player.play("thrust");
+		_jumped = false;
 
 func die() -> void:
 	SignalHub.emit_on_plane_died();
