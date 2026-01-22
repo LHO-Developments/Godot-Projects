a141828828f34a2955183897ae341041295d6ca7
LHO-Developments <LHO_Developments@outlook.com>
2026-01-22 11:31:19 -0600

    setup vars and funcs for animal

--- Angry Animal/angry-animal/Scene/Animal.gd
+++ Angry Animal/angry-animal/Scene/Animal.gd
@@ -1,19 +1,55 @@
 extends RigidBody2D
 class_name Animal;
 
+enum AnimalState {Ready, Drag, Release};
+
+@onready var debug_label: Label = $DebugLabel;
+@onready var arrow: Sprite2D = $Arrow;
+@onready var stretch_sound: AudioStreamPlayer = $StretchSound;
+@onready var launch_sound: AudioStreamPlayer = $LaunchSound;
+@onready var kick_sound: AudioStreamPlayer = $KickSound;
+
+var _state: AnimalState = AnimalState.Ready;
+var _start: Vector2 = Vector2.ZERO;
+var _drag_start: Vector2 = Vector2.ZERO;
+var _dragged_vector: Vector2 = Vector2.ZERO;
+var _arrow_scale: float = 0.0;
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	setup();
 
+func setup() -> void:
+	_arrow_scale = arrow.scale.x;
+	arrow.hide();
+	_start = position;
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	update_debug_label();
 
+func update_debug_label() -> void:
+	var ds: String = "ST:%s SL:%s FR:%s\n" % [
+		AnimalState.keys()[_state], sleeping, freeze
+	]
+	ds += "_drag_start: %.1f, %.1f\n" % [_drag_start.x, _drag_start.y];
+	ds += "_dragged_vector: %.1f, %.1f" % [_dragged_vector.x, _dragged_vector.y];
+	debug_label.text = ds;
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	pass # Replace with function body.
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	pass # Replace with function body.
+
+
+func _on_sleeping_state_changed() -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node) -> void:
 	pass # Replace with function body.
