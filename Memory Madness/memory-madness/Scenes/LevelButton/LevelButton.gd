a43b201d093c66817cf0930b1308cc95c1a9971a
LHO-Developments <LHO_Developments@outlook.com>
2026-02-10 19:51:40 -0600

    Add resource for lvl button

---
+++ Memory Madness/memory-madness/Scenes/LevelButton/LevelButton.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+@export var level_setting: LevelSetting;
+@onready var label: Label = $Label;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	label.text = str(level_setting);
