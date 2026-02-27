51372604242f49f44cfd3c6703d70b7b745ef07b
LHO-Developments <LHO_Developments@outlook.com>
2026-02-26 21:11:35 -0600

    add vertical and axis movement

--- Foxy Adventure/foxy-adventure/Scenes/Player/player.gd
+++ Foxy Adventure/foxy-adventure/Scenes/Player/player.gd
@@ -2,7 +2,10 @@ extends CharacterBody2D
 class_name Player;
 
 @onready var player: CharacterBody2D = $"."
-const _GRAVITY: float = PhysicsServer2D.AREA_PARAM_GRAVITY;
+const _GRAVITY: float = 690.0;
+const  _JUMP_SPEED: float = -270;
+const _RUN_SPEED: float = 200;
+@onready var sprite_2d: Sprite2D = $Sprite2D;
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,9 +15,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	player.velocity.y += _GRAVITY + delta;
+	player.velocity.y += _GRAVITY * delta;
+	
+	if Input.is_action_pressed("jump") && player.is_on_floor():
+		player.velocity.y = _JUMP_SPEED;
+	
+	player.velocity.x = _RUN_SPEED * Input.get_axis("left","right");
+	if not is_equal_approx(player.velocity.x,0.0):
+		sprite_2d.flip_h = player.velocity.x < 0;
+	
 	move_and_slide();
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump") && player.is_on_floor():
-		player.velocity.y -= 50;
+	
