4f614f34c7a09f0a47f67a4bebdf31cc436d9399
LHO-Developments <LHO_Developments@outlook.com>
2026-04-05 16:09:52 -0500

    Add spike ball

--- Foxy Adventure/foxy-adventure/Scenes/Main/main.gd
+++ Foxy Adventure/foxy-adventure/Scenes/Main/main.gd
@@ -1,10 +1,18 @@
 extends Control
+@onready var grid_container: GridContainer = $MarginContainer/GridContainer
+const HIGH_SCORE_DISPLAY = preload("uid://b7gak1nd2l2mi")
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot") == true:
 		GameManager.load_next_level();
 
+func set_scores() -> void:
+	for score: HighScore in GameManager.high_scores.get_scores_list():
+		var hsd: HighScoreDisplayItem = HIGH_SCORE_DISPLAY.instantiate();
+		hsd.setup(score);
+		grid_container.add_child(hsd);
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_scores();
