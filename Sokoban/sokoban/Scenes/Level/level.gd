b66c751aae3bfc6258cab8a0baebcbebabe9840d
LHO-Developments <LHO_Developments@outlook.com>
2026-05-18 13:22:38 -0500

    update camera + add player

--- Sokoban/sokoban/Scenes/Level/level.gd
+++ Sokoban/sokoban/Scenes/Level/level.gd
@@ -7,9 +7,14 @@ const SOURCE_ID: int = 0;
 @onready var walls_tiles: TileMapLayer = $TileLayers/Walls;
 @onready var targets_tiles: TileMapLayer = $TileLayers/Targets;
 @onready var boxes_tiles: TileMapLayer = $TileLayers/Boxes;
+@onready var camera_2d: Camera2D = $Camera2D;
+@onready var player: AnimatedSprite2D = $Player;
 
 
 var _tile_size: int = 0;
+var _player_tile: Vector2i = Vector2i.ZERO;
+
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
@@ -22,6 +27,10 @@ func _ready() -> void:
 	print("Level Loaded: ", GameManager.get_level_Selected());
 	_tile_size = floor_tiles.tile_set.tile_size.x;
 	setup_level();
+
+func place_player_on_tile(title_coord: Vector2i) -> void:
+	player.position = Vector2(title_coord * _tile_size);
+	_player_tile = title_coord;
 
 func get_atlas_cord(lt: TileLayers.LayerType) -> Vector2i:
 	match lt:
@@ -51,6 +60,10 @@ func  clear_tiles() -> void:
 	for tl in tile_layers.get_children():
 		tl.clear();
 
+func move_camera() -> void:
+	var tmr: Rect2i = floor_tiles.get_used_rect();
+	camera_2d.position = tmr.get_center() * _tile_size;
+
 func setup_level() -> void:
 	var level_number: String = GameManager.get_level_Selected();
 	var level_layout: LevelLayout = LevelData.get_level_data(level_number);
@@ -61,4 +74,9 @@ func setup_level() -> void:
 	setup_layer(TileLayers.LayerType.Targets, targets_tiles, level_layout);
 	setup_layer(TileLayers.LayerType.Boxes, boxes_tiles, level_layout);
 	setup_layer(TileLayers.LayerType.TargetBoxes, boxes_tiles, level_layout);
+	
+	
+	place_player_on_tile(level_layout.get_player_start());
+	
+	move_camera();
 	
