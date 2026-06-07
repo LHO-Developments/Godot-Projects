b8b74b5dbab2e377cdbeb67d798b4a9a4d80435e
LHO-Developments <LHO_Developments@outlook.com>
2026-06-07 15:09:49 -0500

    Add pooling on displaying enemies

---
+++ Space Ace/space-ace/Scenes/ObjectContainer/ObjectContainer.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+@export var test_scene: PackedScene
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("test"):
+		on_spawn_pool_object(
+			Vector2(
+				randf_range(400,1000),
+				randf_range(300,700),
+			),
+			test_scene
+		)
+		
+
+func on_spawn_pool_object(position: Vector2, scene: PackedScene) -> void:
+	call_deferred("spawn_deferred", position, scene)
+
+func spawn_deferred(position: Vector2, scene: PackedScene) -> void:
+	var ns: Poolable = scene.instantiate()
+	ns.global_position = position
+	add_child(ns)
+	ns.activate()
