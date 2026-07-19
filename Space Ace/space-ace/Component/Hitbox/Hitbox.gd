d599e99d21208b7db1b43f769ac65709b7ec6ef4
LHO-Developments <LHO_Developments@outlook.com>
2026-07-18 23:05:13 -0500

    Add invincible time for missle

--- Space Ace/space-ace/Component/Hitbox/Hitbox.gd
+++ Space Ace/space-ace/Component/Hitbox/Hitbox.gd
@@ -8,11 +8,16 @@ signal died(collided_with: Area2D);
 signal hit(damage: int);
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var invincibility_timer: Timer = $InvincibilityTimer
 
 @export var max_collisions: int = 1
 @export var damage_dealt: int = 15
 
 var _collisions_left: int = 0
+
+@export var invincible_time: float = 0.1
+
+var _invincible: bool = false
 
 
 @export var collision_shape: Shape2D:
@@ -36,11 +41,18 @@ func _ready() -> void:
 func reset() -> void:
 	print(get_parent().name, " hitbox reset")
 	_collisions_left = max_collisions
+	_invincible = true
+	invincibility_timer.start(invincible_time)
 
 
 func _on_area_entered(area: Area2D) -> void:
+	if _invincible: return
 	if area is HitBox: hit.emit(area.damage_dealt);
 	_collisions_left -= 1
 	if _collisions_left <= 0:
 		print(get_parent().name, " hitbox died")
 		died.emit(area)
+
+
+func _on_invincibility_timer_timeout() -> void:
+	_invincible = false
