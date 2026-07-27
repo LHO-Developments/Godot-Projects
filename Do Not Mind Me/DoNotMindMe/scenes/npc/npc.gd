b98e629b0c3f25387c83b0b3e0f84588e90471b5
LHO-Developments <LHO_Developments@outlook.com>
2026-07-27 00:29:33 -0500

    add npc

---
+++ Do Not Mind Me/DoNotMindMe/scenes/npc/npc.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed: float = 120.0;
+
+@onready var nav_agent: NavigationAgent2D = $NavAgent;
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("set_target"):
+		nav_agent.target_position = get_global_mouse_position();
+
+func _physics_process(delta: float) -> void:
+	if nav_agent.is_navigation_finished(): return
+	
+	var npp: Vector2 = nav_agent.get_next_path_position();
+	var dir: Vector2 = global_position.direction_to(npp);
+	position += dir * speed * delta;
+	rotation = dir.angle();
